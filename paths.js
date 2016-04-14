ab71896ed816f790f53e2e6c6f8da27cd1bf75d6
ScorpOne <milanxx@mail.ru>
2016-04-14 16:50:11 +0300

    Added files via upload

---
+++ paths.js
@@ -0,0 +1,10 @@
+module.exports = {
+    clientPath: 'public',
+    clientCssPath: 'public/css/',
+    clientJsPath: 'public/js/',
+    clientHtmlPath: 'public/html/',
+    redis: {
+        html: '/html/redis.html',
+        def: '/redis'
+    }
+};
