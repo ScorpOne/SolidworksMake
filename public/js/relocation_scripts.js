c3f449aae707e0b92d2af931e053e7354330b0bb
ScorpOne <milanxx@mail.ru>
2016-04-15 13:35:24 +0300

    wrap js code in the anonymous function.

--- public/js/relocation_scripts.js
+++ public/js/relocation_scripts.js
@@ -1,46 +1,47 @@
-// Set's of relocated functions (used in the client's scripts)
+var solidWorks = solidWorks || {};
 
-var SolidWorks = SolidWorks || {}
+(function(){
 
-SolidWorks.relocation = {
+    'use strict';
 
-    modelPath: '/models/',
+    solidWorks.relocation = {
 
-    reloadWithToken: function(newLocation, reloadWindow) {
-        alert(">>> token is: " + newLocation);
-        if (localStorage && "token" in localStorage) {
-            newLocation += ("?token=" + localStorage["token"]);
+        modelPath: '/models/',
+
+        reloadWithToken: function(newLocation, reloadWindow) {
+            alert('>>> token is: ' + newLocation);
+            if (localStorage && 'token' in localStorage) {
+                newLocation += ('?token=' + localStorage['token']);
+            }
+            if (reloadWindow) {
+                window.location.assign(newLocation);
+            }
+            return newLocation;
+        },
+
+
+        stlDownload: function() {
+            var action = modelPath;
+            var paramDict = getUrlVars(window.location.href);
+            if ('stl' in paramDict) {
+                action += paramDict['stl'];
+            }
+            reloadWithToken(action, true);
+        },
+
+
+        addTokenToAction: function(form) {
+            form.action = reloadWithToken(form.action, false);
+        },
+
+
+        saveTokenAndReload: function() {
+            alert('save token and reload');
+            var paramDict = getUrlVars(window.location.href);
+            if ('token' in paramDict) {
+                localStorage['token'] = paramDict['token'];
+            }
+            reloadWithToken('/static/html/index.html', true);
         }
-        if (reloadWindow) {
-            window.location.assign(newLocation);
-        }
-        return newLocation;
-    },
-
-
-    stlDownload: function() {
-        var action = modelPath;
-        var paramDict = getUrlVars(window.location.href);
-        if ("stl" in paramDict) {
-            action += paramDict["stl"];
-        }
-        reloadWithToken(action, true);
-    },
-
-
-    addTokenToAction: function(form) {
-        alert(">>> addTokentoAction");
-        form.action = reloadWithToken(form.action, false);
-    },
-
-
-    saveTokenAndReload: function() {
-        alert(">>> saveTokenAndReload");
-        var paramDict = getUrlVars(window.location.href);
-        if ("token" in paramDict) {
-            localStorage["token"] = paramDict["token"];
-        }
-        reloadWithToken("/static/html/index.html", true);
-    }
-}
-
+    };
+})()
