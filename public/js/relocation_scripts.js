8f7f3ea3eaed2ee61817b5aa06a1b8bdce1b7c84
ScorpOne <milanxx@mail.ru>
2016-04-15 12:07:29 +0300

    first namespaces for js client
    first namespaces for js client in js/relocation_script.js

--- public/js/relocation_scripts.js
+++ public/js/relocation_scripts.js
@@ -1,41 +1,46 @@
 // Set's of relocated functions (used in the client's scripts)
 
-//var tmpHost = 'http://127.0.0.1:8124/';
-var modelPath = '/models/';
+var SolidWorks = SolidWorks || {}
 
-function reloadWithToken(newLocation, reloadWindow) {
-  alert(">>> token is: " + newLocation);
-  if (localStorage && "token" in localStorage) {
-    newLocation += ("?token=" + localStorage["token"]);
-  }
-  if (reloadWindow) {
-    window.location.assign(newLocation);
-  }
-  return newLocation;
+SolidWorks.relocation = {
+
+    modelPath: '/models/',
+
+    reloadWithToken: function(newLocation, reloadWindow) {
+        alert(">>> token is: " + newLocation);
+        if (localStorage && "token" in localStorage) {
+            newLocation += ("?token=" + localStorage["token"]);
+        }
+        if (reloadWindow) {
+            window.location.assign(newLocation);
+        }
+        return newLocation;
+    },
+
+
+    stlDownload: function() {
+        var action = modelPath;
+        var paramDict = getUrlVars(window.location.href);
+        if ("stl" in paramDict) {
+            action += paramDict["stl"];
+        }
+        reloadWithToken(action, true);
+    },
+
+
+    addTokenToAction: function(form) {
+        alert(">>> addTokentoAction");
+        form.action = reloadWithToken(form.action, false);
+    },
+
+
+    saveTokenAndReload: function() {
+        alert(">>> saveTokenAndReload");
+        var paramDict = getUrlVars(window.location.href);
+        if ("token" in paramDict) {
+            localStorage["token"] = paramDict["token"];
+        }
+        reloadWithToken("/static/html/index.html", true);
+    }
 }
-
-
-function stlDownload() {
-  var action = modelPath;
-  var paramDict = getUrlVars(window.location.href);
-  if ("stl" in paramDict) {
-    action += paramDict["stl"];
-  }
-  reloadWithToken(action, true);
-}
-
-
-function addTokenToAction(form) {
-  form.action = reloadWithToken(form.action, false);
-}
-
-
-function saveTokenAndReload() {
-  var paramDict = getUrlVars(window.location.href);
-  if ("token" in paramDict) {
-    localStorage["token"] = paramDict["token"];
-  }
-  reloadWithToken("/static/html/index.html", true);
-}
-
 
